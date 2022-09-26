06894320abd3c24029dd466dec848d1e9e09875b
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-09-26 16:36:40 +0100

    Use database to post and get

--- code/user.py
+++ code/user.py
@@ -1,5 +1,43 @@
+import sqlite3
+
 class User():
     def __init__(self,_id,username,password) -> None:
         self.id=_id
         self.username=username
-        self.password=password
+        self.password=password 
+    @classmethod
+    def find_by_username(cls,username):
+        connection=sqlite3.connect("data.db")
+        cursor=connection.cursor()
+
+        query="SELECT * FROM users WHERE username=?"
+
+        result=cursor.execute(query,(username,))  #
+        #Get the first row 
+        row=result.fetchone()
+
+        if row :
+            user=cls(*row)
+        else:
+            user=None
+        connection.close()
+
+        return user
+    @classmethod
+    def find_by_id(cls,_id):
+        connection=sqlite3.connect("data.db")
+        cursor=connection.cursor()
+
+        query="SELECT * FROM users WHERE id=?"
+
+        result=cursor.execute(query,(_id,))  #
+        #Get the first row 
+        row=result.fetchone()
+
+        if row :
+            user=cls(*row)
+        else:
+            user=None
+        connection.close()
+
+        return user
